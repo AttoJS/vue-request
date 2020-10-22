7108f1af9f79ed503ba03bf744ffbbfe0e94388f
John <John60676@qq.com>
2020-10-22 18:30:47 +0800

    chore: update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-ts-ignore': 'off'
   },
   overrides: [
     {
