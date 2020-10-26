83c522685fdef45dee055ba1987aa15457266a19
BennyEu <bennyyu0429@gmail.com>
2020-10-26 18:14:12 +0800

    test: optimzed code

--- use-request/__tests__/index.test.tsx
+++ use-request/__tests__/index.test.tsx
@@ -25,12 +25,12 @@ describe('useRequest', () => {
         setup() {
           const { data } = useRequest(request);
 
-          return () => <button>{`data, ${data.value}`}</button>;
+          return () => <button>{`data:${data.value}`}</button>;
         },
       }),
     );
     await waitForAll();
-    expect(wrapper.vm.$el.textContent).toBe(`data, success`);
+    expect(wrapper.vm.$el.textContent).toBe('data:success');
   });
 
   test('can be manually triggered', async () => {
@@ -39,14 +39,14 @@ describe('useRequest', () => {
         setup() {
           const { data, run } = useRequest(request, { manual: true });
 
-          return () => <button onClick={() => run.value()}>{`data, ${data.value}`}</button>;
+          return () => <button onClick={() => run.value()}>{`data:${data.value}`}</button>;
         },
       }),
     );
-    expect(wrapper.vm.$el.textContent).toBe(`data, undefined`);
+    expect(wrapper.vm.$el.textContent).toBe('data:undefined');
     await wrapper.find('button').trigger('click');
     await waitForAll();
-    expect(wrapper.vm.$el.textContent).toBe(`data, success`);
+    expect(wrapper.vm.$el.textContent).toBe('data:success');
   });
 
   test('defaultParams should work', async () => {
@@ -57,12 +57,12 @@ describe('useRequest', () => {
             defaultParams: ['hello', 'world'],
           });
 
-          return () => <button>{`data, ${data.value}`}</button>;
+          return () => <button>{`data:${data.value}`}</button>;
         },
       }),
     );
     await waitForAll();
-    expect(wrapper.vm.$el.textContent).toBe(`data, hello,world`);
+    expect(wrapper.vm.$el.textContent).toBe('data:hello,world');
   });
 
   test('run can be accept params', async () => {
@@ -72,14 +72,14 @@ describe('useRequest', () => {
           const { data, run } = useRequest(request);
 
           return () => (
-            <button onClick={() => run.value('hello', 'world')}>{`data, ${data.value}`}</button>
+            <button onClick={() => run.value('hello', 'world')}>{`data:${data.value}`}</button>
           );
         },
       }),
     );
     await wrapper.find('button').trigger('click');
     await waitForAll();
-    expect(wrapper.vm.$el.textContent).toBe(`data, hello,world`);
+    expect(wrapper.vm.$el.textContent).toBe('data:hello,world');
   });
 
   test('mutate should work', async () => {
@@ -88,12 +88,12 @@ describe('useRequest', () => {
         setup() {
           const { data, mutate } = useRequest(request);
 
-          return () => <button onClick={() => mutate.value('ok')}>{`data, ${data.value}`}</button>;
+          return () => <button onClick={() => mutate.value('ok')}>{`data:${data.value}`}</button>;
         },
       }),
     );
     await wrapper.find('button').trigger('click');
-    expect(wrapper.vm.$el.textContent).toBe(`data, ok`);
+    expect(wrapper.vm.$el.textContent).toBe('data:ok');
   });
 
   test('refresh should work', async () => {
@@ -103,14 +103,14 @@ describe('useRequest', () => {
           const { refresh, loading } = useRequest(request);
 
           return () => (
-            <button onClick={() => refresh.value()}>{`loading, ${loading.value}`}</button>
+            <button onClick={() => refresh.value()}>{`loading:${loading.value}`}</button>
           );
         },
       }),
     );
     await wrapper.find('button').trigger('click');
-    expect(wrapper.vm.$el.textContent).toBe('loading, true');
+    expect(wrapper.vm.$el.textContent).toBe('loading:true');
     await waitForAll();
-    expect(wrapper.vm.$el.textContent).toBe('loading, false');
+    expect(wrapper.vm.$el.textContent).toBe('loading:false');
   });
 });
