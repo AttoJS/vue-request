cfa6bc2845efec2ad9018d97a7d84631aadcec97
BennyEu <bennyyu0429@gmail.com>
2020-10-26 18:13:24 +0800

    chore: modify jest config

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,11 @@ module.exports = {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
   collectCoverage: process.env.COVERAGE === 'true',
-  collectCoverageFrom: ['use-request/**/*.{ts,tsx}', '!**/node_modules/**'],
+  collectCoverageFrom: [
+    'use-request/**/*.{ts,tsx}',
+    '!**/node_modules/**',
+    '!use-request/__tests__/**/*',
+  ],
   testMatch: ['<rootDir>/use-request/__tests__/**/*.test.[jt]s?(x)'],
   globals: {
     'ts-jest': {
