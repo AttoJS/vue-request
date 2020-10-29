86623e520960e2b89da6481a7e96b1520e76ca73
BennyEu <bennyyu0429@gmail.com>
2020-10-29 17:58:05 +0800

    fix: useRequest types

--- use-request/index.ts
+++ use-request/index.ts
@@ -2,7 +2,7 @@ import { toRefs } from 'vue';
 import { BaseOptions } from './config';
 import { Query } from './createQuery';
 import useAsyncQuery from './useAsyncQuery';
-import { isPromise } from './utils';
+import { isFunction, isPlainObject, isPromise, isString } from './utils';
 
 export type ServiceParams = string | Record<string, any>;
 export type IService<R, P extends unknown[]> =
@@ -27,39 +27,30 @@ function useRequest<R, P extends unknown[]>(
   const requestMethod = requestProxy;
 
   let promiseQuery: (() => Promise<R>) | ((...args: P) => Promise<R>);
-  switch (typeof service) {
-    case 'string': {
-      promiseQuery = () => requestMethod(service);
-      break;
-    }
-    case 'object': {
-      const { url, ...rest } = service;
-      promiseQuery = () => requestMethod(url, rest);
-      break;
-    }
-    case 'function':
-      promiseQuery = (...args: P) =>
-        new Promise<R>((resolve, reject) => {
-          let _service = service(...args);
-          // 是否为普通异步请求
-          if (!isPromise(_service)) {
-            switch (_service) {
-              case 'string': {
-                _service = () => requestMethod(_service);
-                break;
-              }
-              case 'object': {
-                const { url, ...rest } = _service;
-                _service = () => requestMethod(url, rest);
-                break;
-              }
-            }
+
+  if (isString(service)) {
+    promiseQuery = () => requestMethod(service);
+  } else if (isFunction(service)) {
+    promiseQuery = (...args: P) =>
+      new Promise<R>((resolve, reject) => {
+        let _service = service(...args);
+        // 是否为普通异步请求
+        if (!isPromise(_service)) {
+          if (isPlainObject(_service)) {
+            const { url, ...rest } = _service;
+            _service = () => requestMethod(url, rest);
+          } else if (isString(_service)) {
+            _service = () => requestMethod(_service);
           }
-          _service.then(resolve).catch(reject);
-        });
-      break;
-    default:
-      throw Error('未知service类型');
+        }
+
+        _service.then(resolve).catch(reject);
+      });
+  } else if (isPlainObject(service)) {
+    const { url, ...rest } = service;
+    promiseQuery = () => requestMethod(url, rest);
+  } else {
+    throw Error('未知service类型');
   }
 
   return toRefs(useAsyncQuery<R, P>(promiseQuery, options));
--- use-request/utils/index.ts
+++ use-request/utils/index.ts
@@ -1,3 +1,10 @@
-export const isPromise = (fn: any): fn is Promise<any> => fn instanceof Promise;
+export const objectToString = Object.prototype.toString;
+export const toTypeString = (val: unknown): string => objectToString.call(val);
 
-export const isFunction = (fn: any): fn is Function => fn instanceof Function;
+export const isString = (val: unknown): val is string => toTypeString(val) === '[object String]';
+export const isPlainObject = (val: unknown): val is Record<string, any> =>
+  toTypeString(val) === '[object Object]';
+
+export const isPromise = (fn: unknown): fn is Promise<unknown> => fn instanceof Promise;
+
+export const isFunction = (fn: unknown): fn is Function => fn instanceof Function;
