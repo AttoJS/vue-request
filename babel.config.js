e4c69acb273be058078341c4dc028febc147f065
John <John60676@qq.com>
2020-11-12 14:09:33 +0800

    chore: distinguish babel config by NODE_ENV

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,18 @@
+const presets = ['@babel/preset-env'];
+const plugins = ['@vue/babel-plugin-jsx'];
+
+if (process.env.NODE_ENV === 'test') {
+  plugins.push('@babel/plugin-transform-runtime');
+} else {
+  plugins.push([
+    '@babel/plugin-transform-runtime',
+    {
+      corejs: 3,
+    },
+  ]);
+}
+
 module.exports = {
-  presets: ['@babel/preset-env'],
-  plugins: [
-    '@vue/babel-plugin-jsx',
-    [
-      '@babel/plugin-transform-runtime',
-      {
-        corejs: 3,
-      },
-    ],
-  ],
+  presets,
+  plugins,
 };
