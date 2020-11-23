34c7c37714c5c06463b3accd59ea78d1a9273efc
John60676 <John60676@qq.com>
2020-11-23 15:54:16 +0800

    refactor: formatResult types (#7)

--- src/config.ts
+++ src/config.ts
@@ -40,32 +40,22 @@ export type BaseOptions<R, P extends unknown[]> = GlobalOptions & {
   refreshDeps?: Ref<any>[];
   cacheKey?: string;
   queryKey?: (...args: P) => string;
-  // TODO: 正确处理 formatResult 返回值类型和普通请求返回值类型
-  formatResult?: (data: any) => R;
   onSuccess?: (data: R, params: P) => void;
   onError?: (error: Error, params: P) => void;
 };
+
+export type FormatOptions<R, P extends unknown[], FR> = {
+  formatResult: (data: R) => FR;
+} & BaseOptions<FR, P>;
+
+export type MixinOptions<R, P extends unknown[], FR> = BaseOptions<R, P> | FormatOptions<R, P, FR>;
 
 export type Config<R, P extends unknown[]> = Omit<
   BaseOptions<R, P>,
   'defaultParams' | 'manual' | 'ready' | 'refreshDeps' | 'queryKey'
 > & {
+  formatResult?: (data: any) => R;
   pollingHiddenFlag: Ref<boolean>;
   initialAutoRunFlag: Ref<boolean>;
   updateCache: (state: State<R, P>) => void;
 };
-
-const DefaultOptions: BaseOptions<any, any> = {
-  defaultParams: [],
-  manual: false,
-  ready: ref(true),
-  throwOnError: false,
-  refreshDeps: [],
-  loadingDelay: 0,
-  pollingWhenHidden: false,
-  refreshOnWindowFocus: false,
-  focusTimespan: 5000,
-  cacheTime: 10000,
-  staleTime: 0,
-};
-export default DefaultOptions as Required<BaseOptions<any, any>>;
