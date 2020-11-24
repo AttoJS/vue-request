aa64567a97b2eeaced779a1556d19f3c070e0e0b
John <John60676@qq.com>
2020-11-24 18:49:23 +0800

    refactor: optimize types

--- src/useRequest.ts
+++ src/useRequest.ts
@@ -13,14 +13,12 @@ export type IService<R, P extends unknown[]> =
   | ServiceParams
   | Query<R, P>;
 
-function requestProxy(...args: unknown[]) {
-  // @ts-ignore
-  return fetch(...args).then(res => {
-    if (res.ok) {
-      return res.json();
-    }
-    throw new Error(res.statusText);
-  });
+async function requestProxy(...args: [url: string, ...rest: any[]]) {
+  const res = await fetch(...args);
+  if (res.ok) {
+    return res.json();
+  }
+  throw new Error(res.statusText);
 }
 
 function useRequest<R, P extends unknown[] = any>(service: IService<R, P>): BaseResult<R, P>;
@@ -28,7 +26,7 @@ function useRequest<R, P extends unknown[] = any, FR = any>(
   service: IService<R, P>,
   options: FormatOptions<R, P, FR>,
 ): BaseResult<FR, P>;
-function useRequest<R, P extends unknown[]>(
+function useRequest<R, P extends unknown[] = any>(
   service: IService<R, P>,
   options: BaseOptions<R, P>,
 ): BaseResult<R, P>;
@@ -36,8 +34,6 @@ function useRequest<R, P extends unknown[], FR>(
   service: IService<R, P>,
   options?: MixinOptions<R, P, FR>,
 ) {
-  const requestMethod = requestProxy;
-
   let promiseQuery: (() => Promise<R>) | ((...args: P) => Promise<R>);
 
   if (isFunction(service)) {
@@ -48,9 +44,9 @@ function useRequest<R, P extends unknown[], FR>(
       if (!isPromise(_service)) {
         if (isPlainObject(_service)) {
           const { url, ...rest } = _service;
-          finallyService = requestMethod(url, rest);
+          finallyService = requestProxy(url, rest);
         } else if (isString(_service)) {
-          finallyService = requestMethod(_service);
+          finallyService = requestProxy(_service);
         } else {
           throw new Error('Unknown service type');
         }
@@ -64,9 +60,9 @@ function useRequest<R, P extends unknown[], FR>(
     };
   } else if (isPlainObject(service)) {
     const { url, ...rest } = service;
-    promiseQuery = () => requestMethod(url, rest);
+    promiseQuery = () => requestProxy(url, rest);
   } else if (isString(service)) {
-    promiseQuery = () => requestMethod(service);
+    promiseQuery = () => requestProxy(service);
   } else {
     throw Error('Unknown service type');
   }
