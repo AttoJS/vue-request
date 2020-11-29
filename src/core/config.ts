0a493f2e2a29e36f08e9c411c6be8ec086d636e4
John60676 <John60676@qq.com>
2020-11-29 14:10:37 +0800

    Revert "feat: error retry (#8)"
    This reverts commit b52ccfb833548d2c6fd681abbc4259c7435f882f.

--- src/core/config.ts
+++ src/core/config.ts
@@ -30,9 +30,6 @@ export type GlobalOptions = {
   // -1 means the cache is always valid
   staleTime?: number;
   manual?: boolean;
-  // error retry
-  errorRetryCount?: number;
-  errorRetryInterval?: number;
 };
 
 export type BaseOptions<R, P extends unknown[]> = GlobalOptions & {
