b5239367112b88355f71903a4447481c5e0c5b9a
John <John60676@qq.com>
2021-02-19 15:10:31 +0800

    refactor: optimize the way to generate `service` (#27)

--- src/core/utils/types.ts
+++ src/core/utils/types.ts
@@ -1,4 +1,5 @@
 import { Ref } from 'vue';
+import { Query } from '../createQuery';
 
 export type PartialRecord<T> = {
   [P in keyof T]: Partial<T[P]>;
@@ -13,3 +14,15 @@ export type UnRef<T> = T extends Ref<infer V> ? V : T;
 export type UnWrapRefObject<T> = {
   [P in keyof T]: UnRef<T[P]>;
 };
+
+export type ServiceObject = {
+  [key: string]: any;
+  url: string;
+};
+
+export type ServiceParams = string | ServiceObject;
+
+export type IService<R, P extends unknown[]> =
+  | ((...args: P) => ServiceParams)
+  | ServiceParams
+  | Query<R, P>;
