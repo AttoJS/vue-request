85e2a79440a58355ecb95bc2b8d7f39c6a1c9ecc
John <John60676@qq.com>
2021-02-24 15:45:19 +0800

    docs: restore example

--- example/App.tsx
+++ example/App.tsx
@@ -1,63 +1,23 @@
-import axios from 'axios';
-import { defineComponent, watchEffect } from 'vue';
-import { usePagination } from 'vue-request';
+import { defineComponent } from 'vue';
+import { useRequest } from 'vue-request';
 
-type APIParams = {
-  // results: number;
-  // page?: number;
-  // [key: string]: any;
-};
-type APIResult = {
-  results: {
-    gender: 'female' | 'male';
-    name: string;
-    email: string;
-  }[];
-};
+function testService() {
+  return new Promise<string>(resolve => {
+    setTimeout(() => {
+      resolve('success');
+    }, 1000);
+  });
+}
 
-const queryData = (params: APIParams) => {
-  return axios.get<APIResult>('https://randomuser.me/api', { params: params });
-};
-
-let index = 1;
 export default defineComponent({
   name: 'App',
   setup() {
-    const {
-      changeCurrent,
-      changePageSize,
-      data,
-      loading,
-      current,
-      pageSize,
-      total,
-      totalPage,
-    } = usePagination(queryData, {
-      formatResult: data => data.data,
-      pagination: {
-        currentKey: 'page',
-        pageSizeKey: 'results',
-        totalKey: 'results.0.dob.age',
-        totalPageKey: 'results.0.registered.age',
-      },
-    });
-    watchEffect(() => {
-      console.log(data.value);
-    });
+    const { run, data, loading } = useRequest(testService);
     return () => (
       <div>
-        <button onClick={() => changeCurrent((index += 1))}>
-          changeCurrent
-        </button>
-        <button onClick={() => changePageSize((index += 1))}>
-          changePageSize
-        </button>
+        <button onClick={() => run()}>run</button>
         <br />
-        current: {current.value}, pageSize: {pageSize.value}, total:{' '}
-        {total.value}, totalPage: {totalPage.value}
-        <br />
-        <br />
-        {loading.value ? 'loading...' : JSON.stringify(data.value)}
+        {loading.value ? 'loading...' : data.value}
       </div>
     );
   },
