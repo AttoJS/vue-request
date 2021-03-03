1912af610bf5dcc24aa0a575bf74a24040815258
John <John60676@qq.com>
2021-03-03 16:43:22 +0800

    docs: restore example[skip ci]

--- example/App.tsx
+++ example/App.tsx
@@ -1,35 +1,10 @@
-import { defineComponent, reactive, watchEffect } from 'vue';
-import { useLoadMore } from 'vue-request';
-import Mock from 'mockjs';
+import { defineComponent } from 'vue';
+import { useRequest } from 'vue-request';
 
-function api(current: number, total = 10) {
-  let list: string[] = [];
-  if (current <= total) {
-    list = Mock.mock({
-      'list|5': ['@name'],
-    }).list;
-  } else {
-    list = [];
-  }
-  return {
-    current,
-    total,
-    list,
-  };
-}
-type APIReturnType = ReturnType<typeof api>;
-type rawDataType = {
-  data: APIReturnType;
-  dataList: APIReturnType['list'];
-};
-
-function testService(rawData: rawDataType) {
-  const current = (rawData?.data?.current || 0) + 1;
-  console.log(`${current}`, rawData);
-
-  return new Promise<APIReturnType>(resolve => {
+function testService() {
+  return new Promise<string>(resolve => {
     setTimeout(() => {
-      resolve(api(current));
+      resolve('success');
     }, 1000);
   });
 }
@@ -37,51 +12,12 @@ function testService(rawData: rawDataType) {
 export default defineComponent({
   name: 'App',
   setup() {
-    const { loadMore, loadingMore, dataList, noMore, reload } = useLoadMore<
-      APIReturnType,
-      any,
-      APIReturnType['list']
-    >(testService, {
-      isNoMore: d => {
-        return d?.current >= d?.total;
-      },
-    });
-
-    const testReactive = reactive({ first: 'init' });
-
-    watchEffect(() => {
-      console.log(testReactive);
-    });
-
+    const { run, data, loading } = useRequest(testService);
     return () => (
       <div>
-        <button
-          disabled={noMore.value}
-          onClick={() => {
-            loadMore();
-            Object.keys(testReactive).forEach(key => {
-              testReactive[key] = new Date();
-            });
-          }}
-        >
-          run
-        </button>
-
-        <button
-          onClick={() => {
-            reload();
-          }}
-        >
-          reload
-        </button>
+        <button onClick={() => run()}>run</button>
         <br />
-        {loadingMore.value
-          ? 'loading...'
-          : dataList.value?.map((i, idx) => (
-              <div key={idx}>
-                <h4 style="display: inline-block">{idx}</h4>: {i}
-              </div>
-            ))}
+        {loading.value ? 'loading...' : data.value}
       </div>
     );
   },
