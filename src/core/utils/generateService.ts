9f05505f65b928418a52d8350360a0ce173add54
窗外雨停了Zz <289901948@qq.com>
2021-09-28 09:34:25 +0800

    refactor(generate-service): optimize `generateService` (#68)
    * refactor(generate-service): optimize method
    
    * refactor: update IService types

--- src/core/utils/generateService.ts
+++ src/core/utils/generateService.ts
@@ -7,38 +7,23 @@ import {
 } from './index';
 import type { IService } from './types';
 
-export default <R, P extends unknown[]>(service: IService<R, P>) => {
-  let promiseQuery: (() => Promise<R>) | ((...args: P) => Promise<R>);
-
-  if (isFunction(service)) {
-    promiseQuery = (...args: P) => {
-      const _service = service(...args);
-      let finallyService: Promise<R>;
-      // 是否为普通异步请求
-      if (!isPromise(_service)) {
-        if (isPlainObject(_service)) {
-          const { url, ...rest } = _service;
-          finallyService = requestProxy(url, rest);
-        } else if (isString(_service)) {
-          finallyService = requestProxy(_service);
-        } else {
-          throw new Error('Unknown service type');
-        }
-      } else {
-        finallyService = _service;
-      }
-
-      return new Promise<R>((resolve, reject) => {
-        finallyService.then(resolve).catch(reject);
-      });
-    };
-  } else if (isPlainObject(service)) {
-    const { url, ...rest } = service;
-    promiseQuery = () => requestProxy(url, rest);
-  } else if (isString(service)) {
-    promiseQuery = () => requestProxy(service);
-  } else {
-    throw Error('Unknown service type');
-  }
-  return promiseQuery;
+const generateService = <R, P extends unknown[]>(
+  service: IService<R, P>,
+): (() => Promise<R>) | ((...args: P) => Promise<R>) => {
+  return (...args: P) => {
+    if (isFunction(service)) {
+      return generateService(service(...args))();
+    } else if (isPromise(service)) {
+      return service;
+    } else if (isPlainObject(service)) {
+      const { url, ...rest } = service;
+      return requestProxy(url, rest);
+    } else if (isString(service)) {
+      return requestProxy(service);
+    } else {
+      throw Error('Unknown service type');
+    }
+  };
 };
+
+export default generateService;
