1ab442b5a8fb9750c410fda0be7e124ad8f48e3e
John <John60676@qq.com>
2022-06-23 16:56:02 +0800

    feat: expose some types

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 export { setGlobalOptions } from './core/config';
-export type { Options } from './core/types';
+export type { Options, Service } from './core/types';
 export { clearCache } from './core/utils/cache';
+export type {
+  DataType,
+  LoadMoreBaseOptions,
+  LoadMoreService,
+} from './useLoadMore';
 export { default as useLoadMore } from './useLoadMore';
+export type { PaginationOptions } from './usePagination';
 export { default as usePagination } from './usePagination';
 export { default as useRequest } from './useRequest';
 export { default as useRequestProvider } from './useRequestProvider';
